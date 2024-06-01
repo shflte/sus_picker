f30ee9bfd7ce77acf1675c53d8a47a2720975afa
Jim Chen <shflte1314527@gmail.com>
2024-06-02 01:46:13 +0800

    imposter picker done

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 import discord
-import arrow
+from discord.ext import commands
+import random
 
 load_dotenv()  # Load environment variables from .env file
 
@@ -9,30 +10,44 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.members = True
+intents.messages = True
+intents.message_content = True
 
-bot = commands.Bot(command_prefix='@', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} 前來報到')
 
-@bot.command(name='sus_picker')
-async def sus_picker(ctx, number_of_impostors: int, *members: discord.Member):
-    if number_of_impostors > len(members):
-        await ctx.send(f"就只有{len(members)}個人，是要怎麼有{number_of_impostors}個內鬼")
-        return
+@bot.event
+async def on_message(message):
+    if bot.user.mentioned_in(message) and message.mention_everyone is False:
+        content = message.content.split()[1:]
+        if not content:
+            await message.channel.send('usage: `@sus_picker <內鬼數量> <成員1> <成員2> ...`')
+            return
+        if not content[0].isdigit():
+            await message.channel.send('內鬼數字要放在前面')
+            return
 
-    impostors = random.sample(members, number_of_impostors)
+        number_of_impostors = int(content[0])
+        ctx = await bot.get_context(message)
+        members = [await commands.MemberConverter().convert(ctx, member) for member in content[1:]]
 
-    for impostor in impostors:
-        try:
-            await impostor.send('內鬼')
-        except discord.Forbidden:
-            await ctx.send(f'沒辦法私訊{impostor.mention}')
+        if number_of_impostors > len(members):
+            await message.channel.send(f"就只有 {len(members)} 個人 是要怎麼有 {number_of_impostors} 個內鬼")
+            return
+    
+        impostors = random.sample(members, number_of_impostors)
 
-    await ctx.send(f'內鬼可以看私訊了')
+        for impostor in impostors:
+            try:
+                await impostor.send('內鬼')
+            except discord.Forbidden:
+                await message.channel.send(f'沒辦法私訊 {impostor.mention}')
 
-bot.run(TOKEN)
+        await message.channel.send(f'內鬼可以看私訊了')
+    
+    await bot.process_commands(message)
 
-
-bot.run(TOKEN)
+bot.run(DISCORD_TOKEN)
